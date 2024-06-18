931edec6598f072a021c1d0fb42a0d80d2bb3ee1
zcf0508 <zcf0508@live.com>
2024-06-18 14:26:09 +0800

    feat: auto create database file

--- server/api/init.ts
+++ server/api/init.ts
@@ -1,6 +1,15 @@
+import { existsSync, writeFileSync } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { readSequelize, writeSequelize } from '~/utils/sqllite/models';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineEventHandler(async () => {
+  if (!existsSync(join(__dirname, '../../', 'chattts.db'))) {
+    writeFileSync(join(__dirname, '../../', 'chattts.db'), '');
+  }
+
   await readSequelize.sync();
   await writeSequelize.sync();
   return true;

e4f5d2c67c9017d6821ce397fb4ea1b3083e03dc
zcf0508 <zcf0508@live.com>
2025-01-10 13:16:46 +0800

    chore: ignore

---
+++ server/routes/resources/[name].get.ts
@@ -0,0 +1,29 @@
+import { createReadStream, existsSync } from 'node:fs';
+import { dirname, join } from 'node:path';
+
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const audiosPath = './resources';
+
+export default defineEventHandler(async (event) => {
+  const filename = decodeURIComponent(event.context.params?.name || '');
+
+  try {
+    if (!filename) { throw createError('missing filename path params'); }
+
+    const filePath = join(__dirname, '../../', audiosPath, filename);
+
+    if (existsSync(filePath)) {
+      return sendStream(event, createReadStream(filePath));
+    }
+    else {
+      console.log('文件不存在');
+      return null;
+    }
+  }
+  catch (error) {
+    return null;
+  }
+});
